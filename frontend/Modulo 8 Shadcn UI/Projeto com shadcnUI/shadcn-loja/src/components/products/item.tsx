d2b5e7e53c1f5d3795eff330941d6e26e7e19de4
Borges dev <leosull50@hotmail.com>
2024-05-20 21:36:59 -0300

    zustand

--- frontend/Modulo 8 Shadcn UI/Projeto com shadcnUI/shadcn-loja/src/components/products/item.tsx
+++ frontend/Modulo 8 Shadcn UI/Projeto com shadcnUI/shadcn-loja/src/components/products/item.tsx
@@ -3,6 +3,7 @@ import { Product } from "@/types/product"
 import { Button } from "../ui/button"
 import { useToast } from "../ui/use-toast"
 import { ToastAction } from "@radix-ui/react-toast"
+import { useCartStore } from "@/stores/cart-store"
 
 type Props = {
     item: Product
@@ -10,8 +11,10 @@ type Props = {
 
 export const ProductItem = ( { item } : Props) => {
     const { toast } = useToast()
+    const { upsertCartItem } = useCartStore(state => state)
+
     const handleAddButton = () => {
-        //TODO: add o item no store
+        upsertCartItem(item, 1)
         toast({
             title: 'Adicionado ao carrinho',
             description: item.name,
